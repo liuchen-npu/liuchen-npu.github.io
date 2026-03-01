e64baf89918e6fb6e6a4029e65b39705eed784c9
lc15225888231 <3281686408@qq.com>
2026-03-01 23:32:11 +0800

    Site updated: 2026-03-01 23:32:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/d72de449.html","posts/1e677d67.html","posts/7a6a6b5a.html","posts/d9093649.html","posts/3856b9e0.html","posts/eeaf5d86.html","posts/cfebe72b.html"];function toRandomPost(){
+var posts=["posts/d72de449.html","posts/7a6a6b5a.html","posts/1e677d67.html","posts/eeaf5d86.html","posts/cfebe72b.html","posts/d9093649.html","posts/30a4ae8f.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
